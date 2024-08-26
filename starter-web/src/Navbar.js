9a937b3be833ed5c08fda236cd95e489b8df47d6
Omar-Elgazzar <omar.k.elgazzar@gmail.com>
2024-08-26 17:48:05 +0300

    Learned how to add styles

--- starter-web/src/Navbar.js
+++ starter-web/src/Navbar.js
@@ -4,7 +4,13 @@ const Navbar = () => {
             <h1>Starter React Web</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New Page</a>
+                <a href="/create" style={{
+                    color: "white",
+                    backgroundColor: '#f1356d',
+                    borderRadius: '8px'
+
+
+                }}>New Page</a>
             </div>
         </nav>
     );
