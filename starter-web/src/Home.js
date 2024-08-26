87b839d1b9cb9385f911e516921c19bf1e4e83af
Omar-Elgazzar <omar.k.elgazzar@gmail.com>
2024-08-26 18:58:52 +0300

    Learned reactive components

--- starter-web/src/Home.js
+++ starter-web/src/Home.js
@@ -1,11 +1,17 @@
+import { useState } from 'react'
+
 const Home = () => {
 
+    const [name, setName] = useState('mario');
+
     const handleClick =  () => {
-        console.log('hey')
+        setName('luigi')
+
     }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name }</p>
             <button onClick = {handleClick}>Click Me</button>
         </div>
      );
