5797c6c9d8591105c91b4d40b91ff666f2e6c3a6
Omar-Elgazzar <omar.k.elgazzar@gmail.com>
2024-08-26 16:34:05 +0300

    Learning to make multiple components

--- starter-web/src/App.js
+++ starter-web/src/App.js
@@ -1,20 +1,17 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home'
 
 
 function App() {
-  const title = 'Welcome to my first React Website';
-  const link = "https://www.google.com";
+
 
   return (
     <div className="App">
-      
+      <Navbar></Navbar>
       <div className="content">
-        
+        <Home></Home>
       </div>
-      <h1>{title}</h1>
-      <p>Made by watching NetNinjas React Tutorial</p>
-
-      <a href={link}>Link to Google</a>
     </div>
   );
 }
