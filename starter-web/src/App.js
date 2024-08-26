17ee581c904e2abeaaa5b5deffe917024b082824
Omar-Elgazzar <omar.k.elgazzar@gmail.com>
2024-08-26 16:20:34 +0300

    Learning Components

--- starter-web/src/App.js
+++ starter-web/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const title = 'Welcome to my first React Website';
+  const link = "https://www.google.com";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="content">
+        
+      </div>
+      <h1>{title}</h1>
+      <p>Made by watching NetNinjas React Tutorial</p>
+
+      <a href={link}>Link to Google</a>
     </div>
   );
 }
