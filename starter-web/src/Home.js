ad3379f2a6415bd2e484d82b5384ec8e55384a3d
Omar-Elgazzar <omar.k.elgazzar@gmail.com>
2024-08-26 18:17:10 +0300

    Learned Click Events

--- starter-web/src/Home.js
+++ starter-web/src/Home.js
@@ -1,7 +1,12 @@
 const Home = () => {
+
+    const handleClick =  () => {
+        console.log('hey')
+    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick = {handleClick}>Click Me</button>
         </div>
      );
 }
