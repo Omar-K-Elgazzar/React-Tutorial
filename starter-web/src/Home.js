50a324f4c0faf2283a7f945e58afa8e0b41ffe83
Omar-Elgazzar <omar.k.elgazzar@gmail.com>
2024-08-29 15:12:35 +0300

    Learning to use functions as props

--- starter-web/src/Home.js
+++ starter-web/src/Home.js
@@ -10,10 +10,15 @@ const [blogs, setBlogs] = useState([
     {title: 'Coding made easy', body: 'lorem impsum...', author: 'David P', id: 3}
 ]);
 
+    const handleDelete = (id) =>{
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
 
     return ( 
         <div className="home">
-        <BlogList blogs={blogs} title ="All Blogs"/>
+        <BlogList blogs={blogs} title ="All Blogs" handleDelete ={handleDelete}/>
+        
 
         </div>
      );
