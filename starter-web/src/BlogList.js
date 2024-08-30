aa28f95657e40f148730ab14a0d973968e9f8f99
Omar-Elgazzar <omar.k.elgazzar@gmail.com>
2024-08-30 15:48:35 +0300

    Learned Route Parameters

--- starter-web/src/BlogList.js
+++ starter-web/src/BlogList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom/cjs/react-router-dom";
+
 const BlogList = ({blogs, title}) => {
 
 
@@ -9,8 +11,11 @@ const BlogList = ({blogs, title}) => {
             {blogs.map((blog) => (
 
             <div className="blog-preview" key={blog.id}>
-                <h2>{ blog.title} </h2>
-                <p>Written by: {blog.author}</p>
+                <Link to = {`/blogs/${blog.id}`}>
+                    <h2>{ blog.title} </h2>
+                    <p>Written by: {blog.author}</p>
+                </Link>
+                
             </div>
             ))}
         </div>  
